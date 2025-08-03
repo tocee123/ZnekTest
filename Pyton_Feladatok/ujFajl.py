e4b1641f0f91831bf6bf5de19891136574cb54e0
TamasT <93769005+tocee123@users.noreply.github.com>
2025-08-04 00:04:36 +0200

    komment

--- Pyton_Feladatok/ujFajl.py
+++ Pyton_Feladatok/ujFajl.py
@@ -1,3 +1,15 @@
-print("hello1")
-nev = "Zoli"
-print(f"Szia {nev}")
+def greet(name): 
+    """ This function greets the person passed into the name parameter name: The name of the person to greet :return: A greeting message """ 
+    return f'Hello, {name}!'
+
+def CreateListWithXNumberOfRandomIntegers(x:int) -> list[int]:
+    """ Fuggveny definicioja
+    parameter x: the amount of random numbers
+    :return: not null list of integers"""
+    from random import randint
+    if (x<=0):
+        raise Exception(f"{x}should be positive integer!")
+    return [randint(100, 1000) for _ in range(x)]
+
+listam = CreateListWithXNumberOfRandomIntegers(10)
+print(listam)
